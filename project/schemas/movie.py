df7d8aa81ee2624f28b8e5276a64564b9131467f
Dmitriy Lahmenev <lahmenev@gmail.com>
2022-06-03 15:26:08 +0300

    Подключение связанных словарей  для  фронта

--- project/schemas/movie.py
+++ project/schemas/movie.py
@@ -1,5 +1,8 @@
 from marshmallow import Schema, fields
 
+from project.schemas.derector import DirectorSchema
+from project.schemas.genre import GenreSchema
+
 
 class MovieSchema(Schema):
     id = fields.Int(required=True)
@@ -8,5 +11,7 @@ class MovieSchema(Schema):
     trailer = fields.Str(required=True)
     year = fields.Int(required=True)
     rating = fields.Float(required=True)
-    genre_id = fields.Int(required=True)
-    director_id = fields.Int(required=True)
+    genre = fields.Nested(GenreSchema)
+    director = fields.Nested(DirectorSchema)
+    # genre_id = fields.Int(required=True)
+    # director_id = fields.Int(required=True)
